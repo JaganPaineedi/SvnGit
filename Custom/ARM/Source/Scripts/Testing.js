6bfc81b69512e74eb7948e1c1172e3b8bd4c4039
jthippeswamy <jthippeswamy@c539b1c9-96a0-cc4d-b11b-05feeb0536be>
2019-06-17 13:22:19 +0000

    Implemented new functionality
    git-svn-id: https://streamline100.smartcarenet.com/svn/Trunk@5 c539b1c9-96a0-cc4d-b11b-05feeb0536be

---
+++ Custom/ARM/Source/Scripts/Testing.js
@@ -0,0 +1,10 @@
+﻿function SetScreenSpecificValues(dom, action) {
+    try {
+        $('#TextBox_CustomDocument24HourAccess_PhoneNumber').attr('datatype', 'PhoneNumber');
+
+    }
+    catch (err) {
+        LogClientSideException(err, 'Events');
+    }
+}
+//Testing 
